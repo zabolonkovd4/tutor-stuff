87c7db2ee5f4baaa718b70fac08edee5cef070d0
Dmitri Zabolonkov <dmitri.zabolonkov@yandex.ru>
2021-09-25 16:25:37 +0300

    7th task got en error

--- seventh/main.cpp
+++ seventh/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <random>
 #include <algorithm>
+#include <tuple>
 
 using namespace std;
 
@@ -14,6 +15,7 @@ int main()
     std::mt19937 gen(rd());
 
     // Инициализация матрицы случайными числами
+    vector<tuple<int, int, int>> vt;
     cout << "Матрица (8 х 8) случайных чисел:" << endl;
     for(int i = 0; i < m.size(); i++)
     {
@@ -23,29 +25,46 @@ int main()
             std::uniform_int_distribution<> distr(1, 100); // случайные значения в диапазоне 1, 100
             m[i][j] = (int)distr(gen);
             cout << m[i][j] << '\t';
-        }
-        cout << endl;
-    }
-
-    cout << endl;
-    // Поиск минимального элемента ниже главной диагонали
-    vector<int> v;
-    cout << "Элементы ниже главной диагонали:";
-    for(int i = 0; i < m.size(); i++)
-    {
-        for(int j = 0; j < m.size(); j++)
-        {
             if(i > j)
             {
-                v.push_back(m[i][j]);
-                cout << m[i][j] << '\t';
+                tuple<int, int, int> t = make_tuple(m[i][j], i + 1, j + 1);
+                vt.push_back(t);
             }
         }
         cout << endl;
     }
+    cout << endl;
 
-    int result = *min_element(v.begin(), v.end());
-    cout << "Min элемент: " << result << endl;
+    // Сохраняю элементы ниже главной диагонали
+    vector<int> v;
+    for(int i = 0; i < vt.size(); i++)
+    {
+        v.push_back(get<0>(vt[i]));
+        cout << get<0>(vt[i]) << " ";
+    }
 
+    // Поиск минимального элемента ниже главной диагонали
+    int min_elem = *min_element(v.begin(), v.end());
+    vector<int>::iterator it = std::min_element(v.begin(), v.end());
+    int index = distance(v.begin(), it);
+    int m_i = get<1>(vt[index]);
+    int m_j = get<2>(vt[index]);
+    cout << "\nMin элемент: " << min_elem << endl;
+    cout << m_i << '\t' << m_j << endl;
+
+    cout <<"Меняю элемент С(4,2) = " << m[1][3] << " c элементом С(" << m_j << ',' << m_i << ") = ..." << m[m_j][m_i] << endl;
+    int tmp = m[1][3];
+    m[1][3] = m[m_j][m_i];
+    m[m_j][m_i] = tmp;
+
+    cout << "Матрица (8 х 8) после смены минимального элемента ниже главной диагонали и С42:";
+    for(int i = 0; i < m.size(); i++)
+    {
+        for(int j = 0; j < m.size(); j++)
+        {
+            cout << m[i][j] << '\t';
+        }
+        cout << endl;
+    }
     return 1;
 }
