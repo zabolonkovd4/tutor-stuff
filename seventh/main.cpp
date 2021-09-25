2b317d2427f5395de0ecccdcde95c9be605a7502
Zabolonkov Dmitri <zab413@icloud.com>
2021-09-25 21:05:32 +0300

    7th task reworked. now it works

--- seventh/main.cpp
+++ seventh/main.cpp
@@ -9,20 +9,21 @@ using namespace std;
 
 int main()
 {
+    setlocale(LC_ALL, "rus");
     vector<vector<int>> m;
     m.resize(8);
     std::random_device rd;
     std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distr(1, 100); // случайные значения в диапазоне 1, 100
 
     // Инициализация матрицы случайными числами
     vector<tuple<int, int, int>> vt;
-    cout << "Матрица (8 х 8) случайных чисел:" << endl;
+    wcout << L"Матрица (8 х 8) случайных чисел:" << endl;
     for(int i = 0; i < m.size(); i++)
     {
         m[i].resize(8);
         for(int j = 0; j < m.size(); j++)
         {
-            std::uniform_int_distribution<> distr(1, 100); // случайные значения в диапазоне 1, 100
             m[i][j] = (int)distr(gen);
             cout << m[i][j] << '\t';
             if(i > j)
@@ -38,26 +39,22 @@ int main()
     // Сохраняю элементы ниже главной диагонали
     vector<int> v;
     for(int i = 0; i < vt.size(); i++)
-    {
         v.push_back(get<0>(vt[i]));
-        cout << get<0>(vt[i]) << " ";
-    }
 
     // Поиск минимального элемента ниже главной диагонали
     int min_elem = *min_element(v.begin(), v.end());
     vector<int>::iterator it = std::min_element(v.begin(), v.end());
-    int index = distance(v.begin(), it);
-    int m_i = get<1>(vt[index]);
-    int m_j = get<2>(vt[index]);
-    cout << "\nMin элемент: " << min_elem << endl;
-    cout << m_i << '\t' << m_j << endl;
+    int iter = distance(v.begin(), it);
+    int m_i = get<1>(vt[iter]);
+    int m_j = get<2>(vt[iter]);
+    wcout << L"\nМинимальный элемент ниже главной диагонали = " << min_elem << endl;
 
-    cout <<"Меняю элемент С(4,2) = " << m[1][3] << " c элементом С(" << m_j << ',' << m_i << ") = ..." << m[m_j][m_i] << endl;
+    wcout << L"Меняю элемент С(4,2) = " << m[1][3] << L" c элементом С(" << m_j << ',' << m_i << ") = " << m[m_i - 1][m_j - 1] << endl;
     int tmp = m[1][3];
-    m[1][3] = m[m_j][m_i];
-    m[m_j][m_i] = tmp;
+    m[1][3] = m[m_i - 1][m_j - 1];
+    m[m_i - 1][m_j - 1] = tmp;
 
-    cout << "Матрица (8 х 8) после смены минимального элемента ниже главной диагонали и С42:";
+    wcout << L"Матрица (8 х 8) после смены минимального элемента ниже главной диагонали и С42:" << endl;
     for(int i = 0; i < m.size(); i++)
     {
         for(int j = 0; j < m.size(); j++)
