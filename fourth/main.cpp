c066336cda4613e5c276a5ad69ea819e849a16ce
Zabolonkov Dmitri <zab413@icloud.com>
2021-09-26 03:05:29 +0300

    4th task write result in file added

--- fourth/main.cpp
+++ fourth/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
 
 using namespace std;
 
@@ -11,7 +12,12 @@ int main()
     double y_left = 0.6;
     double y_right = 2.4;
     double y_step = 0.8;
+
+    ofstream file("..\\result.txt");
+    if(!file.is_open()) return 0;
+
     cout << "x\ty\tresult" << endl;
+    file << "x\ty\tresult" << endl;
     for(double y = y_left; y <= y_right; y += y_step)
     {
         double dividend = sqrt(pow(x,3)) * y * y - cos(y);
@@ -19,7 +25,10 @@ int main()
 
         result = dividend / divisor;
         cout << x << "\t" << y << "\t" << result << endl;
+        file << x << "\t" << y << "\t" << result << endl;
     }
+
+    file.close();
 
     return 1;
 }
