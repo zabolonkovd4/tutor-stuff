c675f6ae3a9fbb31e67a0f74f3bf97192fc34326
Zabolonkov Dmitri <zab413@icloud.com>
2022-01-15 22:00:54 +0300

    fixed some errors

--- kresov/variant27/seventh/main.cpp
+++ kresov/variant27/seventh/main.cpp
@@ -6,19 +6,16 @@
 using namespace std;
 int main()
 {
-    // TODO: smth wrong with calculation
-    vector<double> M(24);
-    M[0] = (rand() % 100) + 1;
-    double result = M[0];
+    vector<int> M(25);
+    long long int result = 1;
     srand(time(0));
     printf("\nСгенерированный массив случайных целых чисел M[25]:\n");
     for(int i = 0; i < M.size(); i++)
     {
         M[i] = (rand() % 100) + 1;
+        result = result * M[i];
         printf("%i \t", M[i]);
-        if(i == 0) continue;
-        result *= M[i];
     }
-    printf("\nПроизведение всех элементов массива M[i] = %f", result);
+    printf("\nПроизведение всех элементов массива M[25] = %lli", result);
     return 1;
 }
