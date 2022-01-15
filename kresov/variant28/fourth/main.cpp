01de4a52eb7655c6d72f30d9e8b20e6a96bbd486
Zabolonkov Dmitri <zab413@icloud.com>
2022-01-15 22:01:15 +0300

    fixed some errors

--- kresov/variant28/fourth/main.cpp
+++ kresov/variant28/fourth/main.cpp
@@ -8,7 +8,6 @@ int main()
     double maximum = -INFINITY;
     for(double y = 4; y <= 5; y += 0.15)
     {
-        // TODO: figure out why you've got nan result
         double W = sin(y * y) / asin(3 + y) + pow(3, y);
         cout << "W(y) = " << W << " при y = " << y << endl; 
         if(maximum < W) 
